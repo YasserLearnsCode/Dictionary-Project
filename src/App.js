e30c36b3e25513bee35a0c692c74e3a4b1fe8d56
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-06-28 12:15:27 -0700

    added Dictionary component and search engine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"
-        ></img>
-        <h1>Hello World</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+          <h1>Word look up</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by{" "}
+          <a
+            href="https://yasserkabulimbo.netlify.app/"
+            alt="Front-end developer Yaser Kabulimbo's website"
+          >
+            Yasser Kabulimbo
+          </a>
+          . Is open sourced on{" "}
+          <a href="https://github.com/YasserLearnsCode/Dictionary-Project">
+            Github
+          </a>
+          , and hosted on Netlify.
+        </footer>
+      </div>
     </div>
   );
 }
