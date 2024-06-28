9bcbbd9fcec0cd8711f262ae587422b8e41fa41c
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-06-28 10:23:36 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo img-fluid"
+          alt="logo"
+        ></img>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
