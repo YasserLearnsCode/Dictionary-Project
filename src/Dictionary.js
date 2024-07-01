9e28a41ff24ab2cd6b82fb4110b44f39176fa47b
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-06-30 21:50:28 -0700

    added images API and Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers =
+      "Authorization:       BAk41Nq7R3AiccwTPFQFxgpdahRAW4cnCJBKV25VQkx11rfQkcbetmzY";
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
@@ -30,26 +41,28 @@ export default function Dictionary(props) {
   function load() {
     setLoaded(true);
     search();
-
   }
-if (loaded) {
- return (
-   <div className="Dictionary">
-     <section>
-      <h1>What do you want to look up?</h1>
-       <form onSubmit={handleSubmit}>
-         <input type="search" autoFocus={true} defaultValue={props.defaultKeyword} onChange={handleKeywordChange} />
-       </form>
-       <div className="hint">Example: sunset, wine, plant</div>
-     </section>
-     <Results results={results} />
-   </div>
- );
-    } else {
-      load();
-      return "Loading"
-    }
-  
-
- ;
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              defaultValue={props.defaultKeyword}
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <div className="hint">Example: sunset, wine, plant</div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
